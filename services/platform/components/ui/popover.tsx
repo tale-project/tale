c80250e47a310bb27374f83c1f4bf3ac55ab987e
Israel Iyanda <79730986+Israeltheminer@users.noreply.github.com>
2026-01-05 15:08:25 +0100

    refactor(ui): update date picker format and input ring styles (#91)

--- services/platform/components/ui/popover.tsx
+++ services/platform/components/ui/popover.tsx
@@ -26,7 +26,7 @@ const PopoverContent = React.forwardRef<HTMLDivElement, PopoverContentProps>(
         align={align}
         sideOffset={sideOffset}
         className={cn(
-          'z-50 min-w-[14.5rem] max-w-64 w-auto p-1 rounded-xl ring-1 ring-border bg-popover text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 space-y-2 pb-2',
+          'z-50 min-w-[14.5rem] max-w-64 w-auto p-1 rounded-lg ring-1 ring-border bg-popover text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 space-y-2 pb-2',
           className,
         )}
         {...props}
