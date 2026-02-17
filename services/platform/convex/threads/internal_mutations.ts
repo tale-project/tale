5c5ad7cb323a1ae54a0028f2ca559e3510e30a70
larryro <371767072@qq.com>
2026-02-17 21:26:49 +0800

    chore(platform): clean up sub-threads when parent thread is deleted (#486)

--- services/platform/convex/threads/internal_mutations.ts
+++ services/platform/convex/threads/internal_mutations.ts
@@ -1,6 +1,7 @@
 import { v } from 'convex/values';
 
 import { internalMutation } from '../_generated/server';
+import { cleanupOrphanedSubThreads as cleanupOrphanedSubThreadsHandler } from './cleanup_orphaned_sub_threads';
 import { getOrCreateSubThread } from './get_or_create_sub_thread';
 import { subAgentTypeValidator } from './validators';
 
@@ -20,6 +21,21 @@ export const getOrCreateSubThreadAtomic = internalMutation({
       args.parentThreadId,
       args.subAgentType,
       args.userId,
+    );
+  },
+});
+
+export const cleanupOrphanedSubThreads = internalMutation({
+  args: {
+    parentThreadId: v.string(),
+    subThreadIds: v.array(v.string()),
+  },
+  returns: v.object({ archivedCount: v.number() }),
+  handler: async (ctx, args) => {
+    return await cleanupOrphanedSubThreadsHandler(
+      ctx,
+      args.parentThreadId,
+      args.subThreadIds,
     );
   },
 });
